06f648bb08e1912b58fc88c0c928e84756327480
Bamboozle-jpg <kevin@bachelorclan.com>
2023-12-19 01:08:05 -0800

    Can tell when user doesn't have a collection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import { useNavigate, useLocation } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import AddTask from "../Components/addTask"
 import SetUp from "../Components/setupNew"
 
 function Home() {
   const nav = useNavigate();
   const task = AddTask();
-  const setup = SetUp();
+
+  var err = SetUp();
 
   return (
     <div>
         <h1>
           Main Page
         </h1>
-        {setup}
         {task}
         <button onClick={ () => nav( "/" ) }>Landing Page</button>
     </div>
